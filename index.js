187aa3c785faef1e8b16e2e473030df49775e11a
Arella Gussow <arellaeg@gmail.com>
2018-06-07 07:54:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function iterativeLog(array){
-  array.forEach(callback(element, index, array)=>
+  array.forEach(function callback(element, index, array)=>
     {console.log(`${index}:${element}`)})
 }
